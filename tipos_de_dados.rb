bac7cd0f94ab94eb05efdc0bb6093d58c4f4246d
Arthur Freitas <50297812+arthurBruno@users.noreply.github.com>
2019-12-16 19:57:39 -0300

    Trabalhando com tipos de dados
    Funções que retornam os tipos de dados esperados pelo teste, a fim de entender como estes funcionam em Ruby

---
+++ tipos_de_dados.rb
@@ -0,0 +1,33 @@
+class TiposDeDados
+  def ola_mundo
+    return 'Olá Mundo'
+  end
+
+  def um_numero
+    return 10
+  end
+
+  def um_decimal
+    return 10.0
+  end
+
+  def verdadeiro?
+    true
+  end
+
+  def falso?
+    false
+  end
+
+  def array_vazio
+    array = []
+  end
+
+  def array_com_um_elemento
+    array = ["Code"]
+  end
+
+  def array_com_dois_elementos
+    array = ["Code", "Saga"]
+  end
+end
